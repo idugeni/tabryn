1b3ac9eb0d6331df9d5f221d8e76a8daa09a1b97
Idugeni <officialelsa21@gmail.com>
2026-07-29 01:44:11 +0700

    fix: configure ESLint for TypeScript with Node.js globals
    - Add @typescript-eslint/parser and @typescript-eslint/eslint-plugin
    - Add globals package for Node.js environment definitions
    - Configure parser and globals for src/**/*.ts files
    - Replaces no-unused-vars with @typescript-eslint/no-unused-vars

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,29 @@
 import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
     rules: {
-      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
       "no-console": "off",
     },
   },
